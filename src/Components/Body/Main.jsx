ed5cbe68d80883709f6041c2b3bb8d4ef32a1a50
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-11-05 20:35:36 +0500

    added currentforecast

---
+++ src/Components/Body/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CurrentForecast from './CurrentForecast'
+
+const Body = () => {
+  return (
+    <>
+      <CurrentForecast />
+    </>
+  )
+}
+
+export default Body
