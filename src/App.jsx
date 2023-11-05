7fa5efcecd36eacac2a62ec6679ca9d29f7a08d5
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-11-05 22:26:07 +0500

    formated files

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import Body from './Components/Body/Main'
 import Header from './Components/Header/Main'
 
 const App = () => {
-  return (
-    <>
-      <head>
-        <meta charset='UTF-8' />
-        <link rel='icon' type='image/svg+xml' href='/vite.svg' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <title>React Weather App</title>
-      </head>
-      <body>
-        <Header />
-        <Body />
-      </body>
-    </>
-  )
+	return (
+		<>
+			<head>
+				<meta charset='UTF-8' />
+				<link rel='icon' type='image/svg+xml' href='/vite.svg' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<title>React Weather App</title>
+			</head>
+			<body>
+				<Header />
+				<Body />
+			</body>
+		</>
+	)
 }
 
 export default App
